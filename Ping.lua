7259a8a24cbbab92ffac750949bdb6c8ff20d8f6
apoxhu <48226377+apoxhu@users.noreply.github.com>
2024-04-13 09:28:58 -0700

    Update Ping.lua
    repair tabulation

--- Ping.lua
+++ Ping.lua
@@ -7,45 +7,45 @@
 -- The function will return an object with several functions to get result lines, get the state, or free the object. It is currently not implemented to stop a running executable.
 -- Warning: if the plugin is interrupted before the object is freed it will leave temporary files behind.
 function cmdAsync(cmd)
-	local tmpfile = os.tmpname()
-	
-	os.execute('mkdir '..tmpfile)
-	
-	if HostOS() == 'Windows' then
-		local f = io.open(tmpfile..'/x.bat','w')
-		f:write("@"..cmd.."\n@echo %ERRORLEVEL% > return_value.txt\n@exit")
-		f:close()
-		os.execute('cd /d '..tmpfile..' && start /B x.bat > output.txt')
-	else
-		local f = io.open(tmpfile..'/x.sh','w')
-		f:write("#!/bin/bash\n"..cmd.."\necho $? > return_value.txt\nexit")
-		f:close()
-		os.execute('cd '..tmpfile..' && ./x.sh > output.txt')
-	end
-	
-	local outFile = io.open(tmpfile..'/output.txt', "r")
+    local tmpfile = os.tmpname()
+    
+    os.execute('mkdir '..tmpfile)
+    
+    if HostOS() == 'Windows' then
+        local f = io.open(tmpfile..'/x.bat','w')
+        f:write("@"..cmd.."\n@echo %ERRORLEVEL% > return_value.txt\n@exit")
+        f:close()
+        os.execute('cd /d '..tmpfile..' && start /B x.bat > output.txt')
+    else
+        local f = io.open(tmpfile..'/x.sh','w')
+        f:write("#!/bin/bash\n"..cmd.."\necho $? > return_value.txt\nexit")
+        f:close()
+        os.execute('cd '..tmpfile..' && ./x.sh > output.txt')
+    end
+    
+    local outFile = io.open(tmpfile..'/output.txt', "r")
     local finished = false
-	
-	return {
-		isRunning = function(self)
-			return self:getResult() == true
-		end,
-		getResult = function(self) -- this returns the process result. If nil, the process is still running!
-		    local file = io.open(tmpfile..'/return_value.txt', "r")
-		    if not file then
+    
+    return {
+        isRunning = function(self)
+            return self:getResult() == true
+        end,
+        getResult = function(self) -- this returns the process result. If nil, the process is still running!
+            local file = io.open(tmpfile..'/return_value.txt', "r")
+            if not file then
                 return true 
             end
-		    local result = file:read("*a")
-		    file:close()
-		    return result
-		end,
-		getLine = function(self)
+            local result = file:read("*a")
+            file:close()
+            return result
+        end,
+        getLine = function(self)
             if finished then return nil end
-			if not outFile then -- file did not open yet, try again...
-				outFile = io.open(tmpfile..'/output.txt', "r")
-			end
-			
-			if not outFile then return false end
+            if not outFile then -- file did not open yet, try again...
+                outFile = io.open(tmpfile..'/output.txt', "r")
+            end
+            
+            if not outFile then return false end
 
             local lastPosition = outFile:seek() or 0
             local newEndPosition = outFile:seek("end") or 0
@@ -59,7 +59,7 @@ function cmdAsync(cmd)
             if not self:isRunning() then finished = true; io.close(outFile); outFile = nil end
 
             return content, err
-		end,
+        end,
         free = function()
             if outFile then io.close(outFile) outFile = nil end
             if HostOS() == 'Windows' then
@@ -68,13 +68,13 @@ function cmdAsync(cmd)
                 os.execute('rm -r '..tmpfile)
             end
         end
-	}
+    }
 end
 
 return function(display, host)
     local host = host
     Echo("Pinging "..host.."...")
-	local cmdObj = cmdAsync('ping -n 4 '..host)
+    local cmdObj = cmdAsync('ping -n 4 '..host)
     local result
     repeat
         result = cmdObj:getLine()
