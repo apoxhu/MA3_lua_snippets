e05030434cf299925e3fdc80f5bf1b5099575018
apoxhu <48226377+apoxhu@users.noreply.github.com>
2025-01-22 04:07:37 -0700

    Update FlipTilt.lua

--- FlipTilt.lua
+++ FlipTilt.lua
@@ -1,5 +1,11 @@
 -- Flip selection's DMXInvertTilt
--- Written by adam.pinter@apox.hu
+-- (C) 2024-2025 Adam Pinter - adam.pinter@apox.hu
+
+-- This code is licensed under an adapted version of the Creative Commons Attribution-NonCommercial 4.0 International
+-- (CC BY-NC 4.0) license. It permits users to modify and use the software for personal or professional (for-profit)
+-- purposes, provided that the software itself is not sold, sublicensed, or redistributed for profit. The copyright
+-- notice must always be retained in all copies or derivative works. This software is provided "as-is," without warranty
+-- of any kind. You can read the full license at https://github.com/apoxhu/MA3_lua_snippets/blob/main/LICENSE.md
 
 return function()
     local fixtureIndex, gridX, gridY, gridZ = SelectionFirst()
